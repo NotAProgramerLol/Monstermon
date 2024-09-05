7245115b66ff89450734ed58f75145946998a41c
FreshOliveOil <oliver.boros.nl@gmail.com>
2024-09-05 16:52:42 +0200

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# This is a Pokemon inspired game named Monstermon
+
+The goal of the game is to battle an opponent that has a few Monsters at his side.
---
+++ src/data/monsterdex.py
@@ -0,0 +1,52 @@
+monsters_data = [
+    {
+        "name": "Geralt",
+        "stats": {
+            "hp": 200,
+            "attack": 50,
+            "speed": 50,
+            "mana": 400,
+        },
+        "moves": ["cut", "slash", "shoot"],
+    },
+    {
+        "name": "Zelda",
+        "stats": {
+            "hp": 100,
+            "attack": 30,
+            "speed": 40,
+            "mana": 500,
+        },
+        "moves": ["blast", "punch", "launch"],
+    },
+    {
+        "name": "Steve",
+        "stats": {
+            "hp": 200,
+            "attack": 40,
+            "speed": 45,
+            "mana": 150,
+        },
+        "moves": ["break", "punch", "pound"],
+    },
+    {
+        "name": "Aloy",
+        "stats": {
+            "hp": 150,
+            "attack": 40,
+            "speed": 60,
+            "mana": 120,
+        },
+        "moves": ["shoot", "break", "wiggle"],
+    },
+    {
+        "name": "Monarch",
+        "stats": {
+            "hp": 125,
+            "attack": 40,
+            "speed": 45,
+            "mana": 300,
+        },
+        "moves": ["wiggle", "break", "launch"],
+    },
+]
